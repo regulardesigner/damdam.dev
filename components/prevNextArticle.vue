706f3736385f4ade24a952a909b8d761c99dfa18
Damien Tailhades <damien.tailhades@youboox.fr>
2021-08-11 10:49:02 +0200

    💄  styling blog post navigation

--- components/prevNextArticle.vue
+++ components/prevNextArticle.vue
@@ -4,16 +4,18 @@
       v-if="prev"
       :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
       class="prev"
+      :title="prev.title"
     >
-      {{ prev.title }}
+      Previous <span class="curved-arrrow down-left">&#10150;</span>
     </NuxtLink>
     <span v-else>&nbsp;</span>
     <NuxtLink
       v-if="next"
       :to="{ name: 'blog-slug', params: { slug: next.slug } }"
       class="next"
+      :title="next.title"
     >
-      {{ next.title }}
+      <span class="curved-arrrow up-right">&#10149;</span> Next
     </NuxtLink>
     <span v-else>&nbsp;</span>
   </nav>
@@ -36,13 +38,29 @@
 
 <style scoped>
 .prev-next-section {
+  border-top: 1px dashed rgba(255,255,255,.2);
+  margin-top: 2rem;
+  padding-top: 2rem;
   display: flex;
   justify-content: space-between;
 }
 .prev, .next {
   font-weight: bold;
   font-size: 1.2rem;
+  border-radius: 1rem;
   padding: 1rem;
-  background-color: rgba(255,255,255,.1);
+  background-color: rgba(255,255,255,1);
+  color: #262a36;
+  text-decoration: none;
+}
+
+.prev:hover,
+.next:hover {
+  text-decoration: underline;
+}
+
+.down-left {
+  display: inline-block;
+  transform: rotate(180deg);
 }
 </style>
